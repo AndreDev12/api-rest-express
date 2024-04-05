99fe398dd1fc1a09e9d65c678e4d007e2cd96d54
André Torres <andretorresjava@gmail.com>
2024-04-04 22:50:31 -0500

    Crear rutas GET y POST.

--- app.js
+++ app.js
@@ -1,17 +1,43 @@
+/* eslint-disable comma-dangle */
 const express = require('express');
-const app = express();
+const crypto = require('node:crypto');
 
 const moviesJSON = require('./movies.json');
 
+const app = express();
+app.use(express.json());
 app.disable('x-powered-by');
 const PORT = process.env.PORT ?? 1234;
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hola mundo' });
+app.get('/movies', (req, res) => {
+  const { genre } = req.query;
+  console.log(genre);
+  if (genre) {
+    const filteredMovies = moviesJSON.filter((movie) =>
+      movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
+    );
+    return res.json(filteredMovies);
+  }
+  res.json(moviesJSON);
 });
 
-app.get('/movies', (req, res) => {
-  res.json(moviesJSON);
+app.get('/movies/:id', (req, res) => {
+  const { id } = req.params;
+  const movie = moviesJSON.find((movie) => movie.id === id);
+  if (movie) return res.json(movie);
+  res.status(404).send({ message: 'Movie not found' });
+});
+
+app.post('/movies', (req, res) => {
+  // const { title, year, director, duration, poster, genre } = req.body;
+
+  const newMovie = {
+    id: crypto.randomUUID(),
+    ...req.body,
+    rate: req.body.rate ?? 0, // uuid v4
+  };
+  moviesJSON.push(newMovie);
+  console.log(moviesJSON);
 });
 
 app.listen(PORT, () => {
