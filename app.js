b184c8f646e98db73847407ea69b6b8494cdc9a8
André Torres <andretorresjava@gmail.com>
2024-04-05 23:10:35 -0500

    Validar con zod.

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-/* eslint-disable comma-dangle */
 const express = require('express');
 const crypto = require('node:crypto');
 
 const moviesJSON = require('./movies.json');
+const { validateMovie } = require('./schemas/movies');
+
+const PORT = process.env.PORT ?? 1234;
 
 const app = express();
 app.use(express.json());
 app.disable('x-powered-by');
-const PORT = process.env.PORT ?? 1234;
 
 app.get('/movies', (req, res) => {
   const { genre } = req.query;
-  console.log(genre);
+
   if (genre) {
     const filteredMovies = moviesJSON.filter((movie) =>
       movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
@@ -29,15 +30,32 @@ app.get('/movies/:id', (req, res) => {
 });
 
 app.post('/movies', (req, res) => {
-  // const { title, year, director, duration, poster, genre } = req.body;
+  const result = validateMovie(req.body);
 
+  if (result.error) {
+    // 422 Unprocessable Entity
+    return res.status(400).json({ error: JSON.parse(result.error.message) });
+  }
+
+  // en base de datos
   const newMovie = {
-    id: crypto.randomUUID(),
-    ...req.body,
-    rate: req.body.rate ?? 0, // uuid v4
+    id: crypto.randomUUID(), // uuid v4
+    ...result.data,
   };
+
+  // Esto no sería REST, porque estamos guardando el estado de la aplicación en memoria
   moviesJSON.push(newMovie);
-  console.log(moviesJSON);
+
+  res.status(201).json(newMovie);
+});
+
+app.patch('/movies/:id', (req, res) => {
+  const { id } = req.params;
+
+  const movieIndex = moviesJSON.findIndex((movie) => movie.id === id);
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' });
+  }
 });
 
 app.listen(PORT, () => {
