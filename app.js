cc9729df69967bd45105ac310a2635e6e533042d
André Torres <andretorresjava@gmail.com>
2024-04-21 22:05:14 -0500

    Patrón de diseño: Inyección de dependencias.

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
 import express, { json } from 'express';
 
 import { corsMiddleware } from './middlewares/cors.js';
-import { moviesRouter } from './routes/movies.js';
+import { createMovieRouter } from './routes/movies.js';
 // como leer un json en ESModules
 // import fs from 'node:fs';
 // const movies = JSON.parse(fs.readFileSync('./movies.json', 'utf-8'));
-const PORT = process.env.PORT ?? 1234;
+export const createApp = ({ movieModel }) => {
+  const PORT = process.env.PORT ?? 1234;
 
-const app = express();
-app.use(json());
-app.use(corsMiddleware());
-app.disable('x-powered-by'); // deshabilitar el header X-Powered-By: Express
+  const app = express();
+  app.use(json());
+  app.use(corsMiddleware());
+  app.disable('x-powered-by'); // deshabilitar el header X-Powered-By: Express
 
-// métodos normales: GET/HEAD/POST
-// métodos complejos: PUT/PATCH/DELETE
+  // métodos normales: GET/HEAD/POST
+  // métodos complejos: PUT/PATCH/DELETE
 
-// CORS PRE-Flight
-// OPTIONS
+  // CORS PRE-Flight
+  // OPTIONS
 
-app.use('/movies', moviesRouter);
+  app.use('/movies', createMovieRouter({ movieModel }));
 
-// app.options('/movies/:id', (req, res) => {
-//   const origin = req.header('origin');
-//   if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
-//     res.header('Access-Control-Allow-Origin', origin);
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-//   }
-//   res.send(200);
-// });
+  // app.options('/movies/:id', (req, res) => {
+  //   const origin = req.header('origin');
+  //   if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+  //     res.header('Access-Control-Allow-Origin', origin);
+  //     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  //   }
+  //   res.send(200);
+  // });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port http://localhost:${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server listening on port http://localhost:${PORT}`);
+  });
+};
